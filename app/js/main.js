75926b410df72130afdeaa94d6dd4608f2cbc540
viktoria564 <neciporenkoviktoria7@gmail.com>
2022-10-29 17:59:12 +0300

    added header, added fonts

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
+
+  $('.menu').find('.menu__link').on('click', function () {
+    if ($(this).hasClass('active')) {
+      return;
+    }
+    $('.menu').find('.menu__link').removeClass('active');
+    $(this).addClass('active');
+  });
   
 });
